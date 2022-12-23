3e501e5c98784e19f901310b0269de57794bcabd
evi1Husky <evi1husky0@gmail.com>
2022-12-23 19:26:10 +1000

    volume knob

---
+++ knob.js
@@ -0,0 +1,75 @@
+const knob = document.querySelector('.knob')
+const knobDial = document.querySelector('.knob-dial')
+
+let dialAngle = 180
+let lastX = 0;
+let currentX = 0;
+
+knobDial.style.transform = `rotate(${dialAngle}deg)`;
+
+knob.addEventListener('touchmove', (event) => {
+  knobEvent(~~(event.touches[0].clientX));
+})
+
+knob.addEventListener("mousedown", () => {
+  knob.style.cursor = 'grabbing';
+  knob.onmousemove = (event) => {
+    console.log(event.x);
+      knobEvent(event.x);
+  }
+})
+
+knob.addEventListener("mouseup", () => {
+  knob.style.cursor = 'pointer';
+  knob.onmousemove = null;
+})
+
+knob.addEventListener("mouseleave", () => {
+  knob.onmousemove = null;
+})
+
+function knobEvent(x) {
+  lastX = x
+  if (lastX > currentX) {
+    rotateRight(x)
+  } else if (lastX < currentX){
+    rotateLeft(x)
+  }
+}
+
+function rotateRight(x) {
+  if (dialAngle <= 450) {
+    dialAngle += 7
+    knobDial.style.transform = `rotate(${dialAngle}deg)`;
+    currentX = x;
+  }
+}
+
+function rotateLeft(x) {
+  if (dialAngle >= 90) {
+    dialAngle -= 7
+    knobDial.style.transform = `rotate(${dialAngle}deg)`;
+    currentX = x;
+  }
+}
+
+function reset() {
+  if (dialAngle < 90) {
+    dialAngle = 90
+    currentX = 0
+    lastX = 0
+    knobDial.style.transform = `rotate(${dialAngle}deg)`;
+  } else if (dialAngle > 450) {
+    dialAngle = 450
+    currentX = 0
+    lastX = 0
+    knobDial.style.transform = `rotate(${dialAngle}deg)`;
+  }
+}
+
+function getCurrentValue() {
+  let knobValuePercent = ~~((dialAngle - 90) * 124 / 450);
+  if (knobValuePercent === -1) {
+    knobValuePercent = 0;
+  }
+}
